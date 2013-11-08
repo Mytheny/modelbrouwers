66d37b17afd2a5b09d0e99de7b36f5bf0bc3af85
Sergei Maertens <sergeimaertens@gmail.com>
2013-11-09 00:09:03 +0100

    Disabled sessionprofile for Travis

--- brouwers/settings/travis.py
+++ brouwers/settings/travis.py
@@ -9,3 +9,26 @@ from base import *
 
 # for ease, override here. No production values are used anyway
 SECRET_KEY = '04wb^^4e5%djxrpe4_u6*&ska+s(j1=!k2^frlfux2se#$%mox'
+
+
+# FIXME: get sessionprofile install working on Travis...
+
+THIRD_PARTY_APPS = (
+    'django_extensions',
+    'django_nose',
+    'south',
+    'tastypie',
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'albums.middleware.UploadifyMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'banning.middleware.BanningMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
